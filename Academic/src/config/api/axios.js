f771d69cf0d8f2e1d5048e52f7ec720a077fe918
Ravi Kumar <156072022+Rkjangir26@users.noreply.github.com>
2024-05-12 16:03:40 +0530

    Update axios.js

--- Academic/src/config/api/axios.js
+++ Academic/src/config/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default axios.create({
   baseURL:
    
- "http://localhost:3500",
+ "https://college-academic-project-backend.vercel.app/",
 
   headers: { "Content-Type": "application/json" },
 });
